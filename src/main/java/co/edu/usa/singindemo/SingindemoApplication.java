5dce7c6668e5f6996a58a0bfce01a11d56effd74
julianpizarro <julian.pizarro.mt@correo.usa.edu.co>
2021-10-15 20:37:11 -0500

    Commit incial

---
+++ src/main/java/co/edu/usa/singindemo/SingindemoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usa.singindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingindemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingindemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/usa/singindemo/SingindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usa.singindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
